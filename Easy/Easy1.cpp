bc84bde68c1ad7e79712475773a691aa89fde8b8
Ryan <ryan.dougherty@asu.edu>
2013-12-06 15:50:33 -0700

    Easy #1 Extra Credit functionality added

--- Easy/Easy1.cpp
+++ Easy/Easy1.cpp
@@ -1,13 +1,35 @@
+/* 
+ * Author: Ryan Dougherty
+ *
+ * Description:
+ * create a program that will ask the users name, age, and reddit username. have it tell them the information back, in the format:
+ * your name is (blank), you are (blank) years old, and your username is (blank)
+ * for extra credit, have the program log this information in a file to be accessed later.
+ *
+ */
+
+#include <fstream>
 #include <iostream>
 #include <string>
 
+const char* fileLocation = "file.txt";
+
 int main() {
-	std::string name, yearsOld, username;
+	std::ifstream inFile(fileLocation);
+	if (inFile) {
+		std::cout << &inFile;
+		return EXIT_SUCCESS;
+	}
+	std::string name, yearsOld, username, stringToWrite;
 	std::cout << "Enter your name: ";
 	std::cin >> name;
 	std::cout << "Enter how old you are: ";
 	std::cin >> yearsOld;
 	std::cout << "Enter your username: ";
 	std::cin >> username;
-	std::cout << "your name is " << name << ", you are " << yearsOld << " years old, and your username is " << username;
+	stringToWrite += "your name is " + name + ", you are " + yearsOld + " years old, and your username is " + username;
+	
+	std::cout << stringToWrite;
+	std::ofstream outFile(fileLocation);
+	outFile << stringToWrite;
 }
