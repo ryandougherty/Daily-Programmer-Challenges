4a62d97d042e51e10f2a9ed6d40fdbbb27292ea4
Ryan Dougherty <redoughe@asu.edu>
2014-01-07 15:04:51 -0700

    Easy 147: Haskell

---
+++ Easy/Easy147.hs
@@ -0,0 +1,11 @@
+validScoreNoSafeties :: Int -> String
+validScoreNoSafeties v
+	| v > 5 = "Valid Score"
+	| v < 1 = "Valid Score"
+	| v == 3 = "Valid Score"
+	| otherwise = "Invalid Score"
+
+validScoreWithSafeties :: Int -> String
+validScoreWithSafeties v
+	| v == 1 = "Invalid Score"
+	| otherwise = "Valid Score"
